4cd1c59379dbc62dfd5979b1187da2b408634bd0
Xianzhan <424447832@qq.com>
2017-06-10 23:14:12 +0800

    update: StringUtil.isEmpty()

--- tool/src/main/java/com/github/xianzhan/tool/util/StringUtil.java
+++ tool/src/main/java/com/github/xianzhan/tool/util/StringUtil.java
@@ -12,6 +12,12 @@ public class StringUtil {
 
     private static final String IPv4 = "((?:(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d)))\\.){3}(?:25[0-5]|2[0-4]\\d|((1\\d{2})|([1-9]?\\d))))";
 
+    /**
+     * 检查字符串是否为 IP 地址
+     *
+     * @param value
+     * @return
+     */
     public static boolean isIP(String value) {
         return isEmpty(value) ? false
                 : Pattern.compile(IPv4).matcher(value).find();
@@ -25,167 +31,6 @@ public class StringUtil {
      * @return
      */
     public static boolean isEmpty(String value) {
-        return isEmpty(value, false);
-    }
-
-    /**
-     * 如果为null，转换为""
-     *
-     * @param value
-     * @return
-     */
-    public static String nullSafeString(String value) {
-        return value == null ? "" : value;
-    }
-
-    /**
-     * 检查字符串是否为空
-     * <p>为null或者长度为0视为空字符串
-     *
-     * @param value     要检查的字符串
-     * @param trim      是否去掉头尾的特定字符
-     * @param trimChars 要去掉的特定字符
-     * @return
-     */
-    public static boolean isEmpty(String value, boolean trim, char... trimChars) {
-        if (trim)
-            return value == null || trim(value, trimChars).length() <= 0;
-        return value == null || value.length() <= 0;
-    }
-
-    /**
-     * 检查字符串是否为空
-     * <p>为null或者长度为0视为空字符串
-     *
-     * @param value 要检查的字符串
-     * @param trim  是否去掉头尾的空格
-     * @return
-     */
-    public static boolean isEmpty(String value, boolean trim) {
-        return isEmpty(value, trim, ' ');
-    }
-
-    /**
-     * 确保存入数据库的string值不会引起数据库报错。
-     * <p>
-     * 1. 数据库不允许为null，value为nul时返回""；<br />
-     * 2. 超过最大长度时截断字符串。
-     *
-     * @param value     要存入数据库的字符串值。
-     * @param nullAble  是否允许为null。
-     * @param maxLength 最大长度。
-     * @return
-     */
-    public static String dbSafeString(String value, boolean nullAble, int maxLength) {
-        if (value == null) {
-            if (nullAble)
-                return null;
-            return nullSafeString(value);
-        }
-        if (value.length() > maxLength)
-            return value.substring(0, maxLength);
-        return value;
-    }
-
-    /**
-     * 去掉头尾空格字符
-     *
-     * @param value 待处理的字符串
-     * @return
-     */
-    public static String trim(String value) {
-        return trim(3, value, ' ');
-    }
-
-    /**
-     * 去除字符串头尾的特定字符
-     *
-     * @param value 待处理的字符串
-     * @param chars 需要去掉的特定字符
-     * @return
-     */
-    public static String trim(String value, char... chars) {
-        return trim(3, value, chars);
-    }
-
-    /**
-     * 去除字符串头部的特定字符
-     *
-     * @param value 待处理的字符串
-     * @param chars 需要去掉的特定字符
-     * @return
-     */
-    public static String trimStart(String value, char... chars) {
-        return trim(1, value, chars);
-    }
-
-    /**
-     * 去除字符串尾部的特定字符
-     *
-     * @param value 待处理的字符串
-     * @param chars 需要去掉的特定字符
-     * @return 去掉特定字符的字符串
-     */
-    public static String trimEnd(String value, char... chars) {
-        return trim(2, value, chars);
-    }
-
-    /**
-     * 去掉字符串头尾特定字符
-     *
-     * @param mode  <li>1: 去掉头部特定字符；
-     *              <li>2: 去掉尾部特定字符；
-     *              <li>3: 去掉头尾特定字符；
-     * @param value 待处理的字符串
-     * @param chars 需要去掉的特定字符
-     * @return 去掉特定字符的字符串
-     */
-    private static String trim(int mode, String value, char... chars) {
-        if (value == null || value.length() <= 0)
-            return value;
-
-        int startIndex = 0, endIndex = value.length(), index = 0;
-        if (mode == 1 || mode == 3) {
-            // trim头部
-            while (index < endIndex) {
-                if (contains(chars, value.charAt(index++))) {
-                    startIndex++;
-                    continue;
-                }
-                break;
-            }
-        }
-
-        if (startIndex >= endIndex)
-            return "";
-
-        if (mode == 2 || mode == 3) {
-            // trim尾部
-            index = endIndex - 1;
-            while (index >= 0) {
-                if (contains(chars, value.charAt(index--))) {
-                    endIndex--;
-                    continue;
-                }
-                break;
-            }
-        }
-
-        if (startIndex >= endIndex)
-            return "";
-        if (startIndex == 0 && endIndex == value.length() - 1)
-            return value;
-
-        return value.substring(startIndex, endIndex);
-    }
-
-    private static boolean contains(char[] chars, char chr) {
-        if (chars == null || chars.length <= 0)
-            return false;
-        for (char aChar : chars) {
-            if (aChar == chr)
-                return true;
-        }
-        return false;
+        return value == null || "".equals(value);
     }
 }
