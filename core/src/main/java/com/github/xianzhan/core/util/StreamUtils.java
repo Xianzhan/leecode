5148bc33b971cc903a2a9a6c3e535dfdda88f1a5
Xianzhan <424447832@qq.com>
2017-06-18 22:33:47 +0800

    add: StreamUtils

---
+++ core/src/main/java/com/github/xianzhan/core/util/StreamUtils.java
@@ -0,0 +1,56 @@
+package com.github.xianzhan.core.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * 描述：流工具类
+ *
+ * @author Lee
+ * @since 2017/6/18
+ */
+public class StreamUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StringUtils.class);
+
+    /**
+     * 字节转对象
+     *
+     * @param bytes
+     * @return
+     */
+    public static Object streamToObjectByJava(byte[] bytes) {
+        Object obj = null;
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais);
+
+            obj = ois.readObject();
+            ois.close();
+            bais.close();
+        } catch (Exception e) {
+            LOGGER.error("[StreamUtils][streamToObjectByJava]Exception: ", e);
+        }
+        return obj;
+    }
+
+    public static byte[] objectToBytesByJava(Object obj) {
+        byte[] bytes = null;
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(obj);
+            bytes = baos.toByteArray();
+            oos.close();
+            baos.close();
+        } catch (Exception e) {
+            LOGGER.error("[StreamUtils][objectToBytesByJava]Exception: ", e);
+        }
+        return bytes;
+    }
+}
