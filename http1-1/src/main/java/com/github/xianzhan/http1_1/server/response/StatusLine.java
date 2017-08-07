7c8e461b8ce5e79179488b3ca08082fdaa855e70
Xianzhan <424447832@qq.com>
2017-08-07 23:46:33 +0800

    update: StatusLine

--- http1-1/src/main/java/com/github/xianzhan/http1_1/server/response/StatusLine.java
+++ http1-1/src/main/java/com/github/xianzhan/http1_1/server/response/StatusLine.java
@@ -1,5 +1,9 @@
 package com.github.xianzhan.http1_1.server.response;
 
+import com.github.xianzhan.http1_1.core.HTTPStatusCode;
+import com.github.xianzhan.http1_1.core.ProtocolVersion;
+import com.github.xianzhan.http1_1.core.Space;
+
 /**
  * 描述：状态行
  *
@@ -7,5 +11,33 @@ package com.github.xianzhan.http1_1.server.response;
  * @since 2017/8/6
  */
 public class StatusLine {
+
+    private ProtocolVersion protocolVersion;
+    private Space space;
+    private HTTPStatusCode httpStatusCode;
+
+    public ProtocolVersion getProtocolVersion() {
+        return protocolVersion;
+    }
+
+    public void setProtocolVersion(ProtocolVersion protocolVersion) {
+        this.protocolVersion = protocolVersion;
+    }
+
+    public Space getSpace() {
+        return space;
+    }
+
+    public void setSpace(Space space) {
+        this.space = space;
+    }
+
+    public HTTPStatusCode getHttpStatusCode() {
+        return httpStatusCode;
+    }
+
+    public void setHttpStatusCode(HTTPStatusCode httpStatusCode) {
+        this.httpStatusCode = httpStatusCode;
+    }
 
 }
