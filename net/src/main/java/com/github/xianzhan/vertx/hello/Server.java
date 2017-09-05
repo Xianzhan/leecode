9c6ec6c51837d0d5ae1876e5b099b2b32ee905d5
Xianzhan <424447832@qq.com>
2017-09-05 23:47:30 +0800

    add: Vert.x

---
+++ net/src/main/java/com/github/xianzhan/vertx/hello/Server.java
@@ -0,0 +1,25 @@
+package com.github.xianzhan.vertx.hello;
+
+import com.github.xianzhan.vertx.util.Runner;
+import io.vertx.core.AbstractVerticle;
+
+/**
+ * 描述：Vertx Hello World Server
+ *
+ * @author Lee
+ * @since 2017/9/5
+ */
+public class Server extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response().putHeader("content-type", "text/html")
+                    .end("<html><body><h1>Hello World</h1></body></html>");
+        }).listen(8080);
+    }
+
+    public static void main(String[] args) {
+        Runner.runExample(Server.class);
+    }
+}
