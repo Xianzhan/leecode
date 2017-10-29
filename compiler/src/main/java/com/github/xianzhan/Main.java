35a3f2458e07094976e72c1be62bba4f967e8709
Xianzhan <424447832@qq.com>
2017-10-29 22:51:15 +0800

    add: SortUtils

--- compiler/src/main/java/com/github/xianzhan/Main.java
+++ compiler/src/main/java/com/github/xianzhan/Main.java
@@ -1,5 +1,14 @@
 package com.github.xianzhan;
 
+import com.github.xianzhan.analysis.exception.LexicalAnalysisException;
+import com.github.xianzhan.analysis.exception.SyntacticAnalysisException;
+import com.github.xianzhan.intermediate.IntermediateCodeExpression;
+import com.github.xianzhan.vm.VirtualMachine;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * 描述：
  *
@@ -8,4 +17,22 @@ package com.github.xianzhan;
  */
 public class Main {
 
+    public static void main(String[] args) {
+        VirtualMachine vm = VirtualMachine.instance();
+
+        try {
+            FileReader reader = new FileReader("system/lib/Array.t");
+            vm.run(reader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (LexicalAnalysisException e) {
+            e.printStackTrace();
+        } catch (SyntacticAnalysisException e) {
+            e.printStackTrace();
+        } catch (IntermediateCodeExpression intermediateCodeExpression) {
+            intermediateCodeExpression.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
