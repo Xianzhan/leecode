e6471e105b3e6f78e5953ac993cb85c69efd6936
xianzhan <424447832@qq.com>
2018-08-29 00:01:57 +0800

    fix: FindBugs-IDEA

--- architecture/src/main/java/architecture/helper/UploadHelper.java
+++ architecture/src/main/java/architecture/helper/UploadHelper.java
@@ -1,26 +1,5 @@
 package architecture.helper;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import architecture.bean.FileParam;
 import architecture.bean.FormParam;
 import architecture.bean.Param;
@@ -28,130 +7,139 @@ import architecture.util.CollectionUtil;
 import architecture.util.FileUtil;
 import architecture.util.StreamUtil;
 import architecture.util.StringUtil;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 文件上传助手类
- * 
- * @author Lee
  *
+ * @author Lee
  */
 public final class UploadHelper {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(UploadHelper.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(UploadHelper.class);
 
-	/**
-	 * Apache Commons FileUpload 提供的 Servlet 文件上传对象
-	 */
-	private static ServletFileUpload servletFileUpload;
+    /**
+     * Apache Commons FileUpload 提供的 Servlet 文件上传对象
+     */
+    private static ServletFileUpload servletFileUpload;
 
-	public static void init(ServletContext servletContext) {
-		File repository = (File) servletContext
-				.getAttribute("javax.servlet.context.tempdir");
-		servletFileUpload = new ServletFileUpload(new DiskFileItemFactory(
-				DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD, repository));
-		int uploadLimit = ConfigHelper.getAppUploadLimit();
-		if (0 != uploadLimit) {
-			servletFileUpload.setFileSizeMax(uploadLimit * 1024 * 1024);
-		}
-	}
+    public static void init(ServletContext servletContext) {
+        File repository = (File) servletContext
+                .getAttribute("javax.servlet.context.tempdir");
+        servletFileUpload = new ServletFileUpload(new DiskFileItemFactory(
+                DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD, repository));
+        int uploadLimit = ConfigHelper.getAppUploadLimit();
+        if (0 != uploadLimit) {
+            servletFileUpload.setFileSizeMax(uploadLimit * 1024 * 1024L);
+        }
+    }
 
-	/**
-	 * 判断请求是否为 multipart 类型
-	 * 
-	 * @param request
-	 * @return
-	 */
-	public static boolean isMultipart(HttpServletRequest request) {
-		return ServletFileUpload.isMultipartContent(request);
-	}
+    /**
+     * 判断请求是否为 multipart 类型
+     *
+     * @param request
+     * @return
+     */
+    public static boolean isMultipart(HttpServletRequest request) {
+        return ServletFileUpload.isMultipartContent(request);
+    }
 
-	/**
-	 * 创建请求对象
-	 * 
-	 * @param request
-	 * @return
-	 * @throws IOException
-	 */
-	public static Param createParam(HttpServletRequest request)
-			throws IOException {
-		List<FormParam> formParamList = new ArrayList<>();
-		List<FileParam> fileParamList = new ArrayList<>();
-		try {
-			Map<String, List<FileItem>> fileItemListMap = servletFileUpload
-					.parseParameterMap(request);
-			if (CollectionUtil.isNotEmpty(fileItemListMap)) {
-				for (Map.Entry<String, List<FileItem>> fileItemListEntry : fileItemListMap
-						.entrySet()) {
-					String fieldName = fileItemListEntry.getKey();
-					List<FileItem> fileItemList = fileItemListEntry.getValue();
-					if (CollectionUtil.isNotEmpty(fileItemList)) {
-						for (FileItem fileItem : fileItemList) {
-							if (fileItem.isFormField()) {
-								String fieldValue = fileItem.getString("UTF-8");
-								formParamList.add(
-										new FormParam(fieldName, fieldValue));
-							} else {
-								String fileName = FileUtil
-										.getRealFileName(new String(
-												fileItem.getName().getBytes(),
-												"UTF-8"));
-								if (StringUtil.isNotEmpty(fileName)) {
-									long fileSize = fileItem.getSize();
-									String contentType = fileItem
-											.getContentType();
-									InputStream inputStream = fileItem
-											.getInputStream();
-									fileParamList.add(new FileParam(fieldName,
-											fileName, fileSize, contentType,
-											inputStream));
-								}
-							}
-						}
-					}
-				}
-			}
-		} catch (FileUploadException e) {
-			LOGGER.error("create param failure", e);
-			throw new RuntimeException(e);
-		}
-		return new Param(formParamList, fileParamList);
-	}
+    /**
+     * 创建请求对象
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    public static Param createParam(HttpServletRequest request)
+            throws IOException {
+        List<FormParam> formParamList = new ArrayList<>();
+        List<FileParam> fileParamList = new ArrayList<>();
+        try {
+            Map<String, List<FileItem>> fileItemListMap = servletFileUpload
+                    .parseParameterMap(request);
+            if (CollectionUtil.isNotEmpty(fileItemListMap)) {
+                for (Map.Entry<String, List<FileItem>> fileItemListEntry : fileItemListMap
+                        .entrySet()) {
+                    String fieldName = fileItemListEntry.getKey();
+                    List<FileItem> fileItemList = fileItemListEntry.getValue();
+                    if (CollectionUtil.isNotEmpty(fileItemList)) {
+                        for (FileItem fileItem : fileItemList) {
+                            if (fileItem.isFormField()) {
+                                String fieldValue = fileItem.getString("UTF-8");
+                                formParamList.add(
+                                        new FormParam(fieldName, fieldValue));
+                            } else {
+                                String fileName = FileUtil.getRealFileName(fileItem.getName());
+                                if (StringUtil.isNotEmpty(fileName)) {
+                                    long fileSize = fileItem.getSize();
+                                    String contentType = fileItem
+                                            .getContentType();
+                                    InputStream inputStream = fileItem
+                                            .getInputStream();
+                                    fileParamList.add(new FileParam(fieldName,
+                                            fileName, fileSize, contentType,
+                                            inputStream));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (FileUploadException e) {
+            LOGGER.error("create param failure", e);
+            throw new RuntimeException(e);
+        }
+        return new Param(formParamList, fileParamList);
+    }
 
-	/**
-	 * 上传文件
-	 */
-	public static void uploadFile(String basePath, FileParam fileParam) {
-		try {
-			if (null != fileParam) {
-				String filePath = basePath + fileParam.getFileName();
-				FileUtil.createFile(filePath);
-				InputStream inputStream = new BufferedInputStream(
-						fileParam.getInputStream());
-				OutputStream outputStream = new BufferedOutputStream(
-						new FileOutputStream(filePath));
-				StreamUtil.copyStream(inputStream, outputStream);
-			}
-		} catch (Exception e) {
-			LOGGER.error("upload file failure", e);
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * 上传文件
+     */
+    public static void uploadFile(String basePath, FileParam fileParam) {
+        try {
+            if (null != fileParam) {
+                String filePath = basePath + fileParam.getFileName();
+                FileUtil.createFile(filePath);
+                InputStream inputStream = new BufferedInputStream(
+                        fileParam.getInputStream());
+                OutputStream outputStream = new BufferedOutputStream(
+                        new FileOutputStream(filePath));
+                StreamUtil.copyStream(inputStream, outputStream);
+            }
+        } catch (Exception e) {
+            LOGGER.error("upload file failure", e);
+            throw new RuntimeException(e);
+        }
+    }
 
-	/**
-	 * 批量上传文件
-	 */
-	public static void uploadFile(String basePath,
-			List<FileParam> fileParamList) {
-		try {
-			if (CollectionUtil.isNotEmpty(fileParamList)) {
-				for (FileParam fileParam : fileParamList) {
-					uploadFile(basePath, fileParam);
-				}
-			}
-		} catch (Exception e) {
-			LOGGER.error("upload files failure", e);
-			throw new RuntimeException(e);
-		}
-	}
+    /**
+     * 批量上传文件
+     */
+    public static void uploadFile(String basePath,
+                                  List<FileParam> fileParamList) {
+        try {
+            if (CollectionUtil.isNotEmpty(fileParamList)) {
+                for (FileParam fileParam : fileParamList) {
+                    uploadFile(basePath, fileParam);
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("upload files failure", e);
+            throw new RuntimeException(e);
+        }
+    }
 }
--- architecture/src/main/java/architecture/util/ClassUtil.java
+++ architecture/src/main/java/architecture/util/ClassUtil.java
@@ -1,7 +1,9 @@
 package architecture.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 import java.net.JarURLConnection;
 import java.net.URL;
@@ -11,135 +13,132 @@ import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * 类操作工具类
- * 
- * @author Lee
  *
+ * @author Lee
  */
 public class ClassUtil {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(ClassUtil.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(ClassUtil.class);
 
-	/**
-	 * 获取类加载器
-	 * 
-	 * @return
-	 */
-	public static ClassLoader getClassLoader() {
-		return Thread.currentThread().getContextClassLoader();
-	}
+    /**
+     * 获取类加载器
+     *
+     * @return
+     */
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
 
-	/**
-	 * 加载类
-	 * 
-	 * @param className
-	 * @param isInitialized
-	 * @return
-	 */
-	public static Class<?> loadClass(String className, boolean isInitialized) {
-		Class<?> cls = null;
+    /**
+     * 加载类
+     *
+     * @param className
+     * @param isInitialized
+     * @return
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls = null;
 
-		try {
-			cls = Class.forName(className, isInitialized, getClassLoader());
-		} catch (ClassNotFoundException e) {
-			LOGGER.error("load class failure", e);
-			throw new RuntimeException(e);
-		}
+        try {
+            cls = Class.forName(className, isInitialized, getClassLoader());
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("load class failure", e);
+            throw new RuntimeException(e);
+        }
 
-		return cls;
-	}
+        return cls;
+    }
 
-	/**
-	 * 获取指定包名下的所有类
-	 * 
-	 * @param packageName
-	 * @return
-	 */
-	public static Set<Class<?>> getClassSet(String packageName) {
-		Set<Class<?>> classSet = new HashSet<>();
-		try {
-			Enumeration<URL> urls = getClassLoader()
-					.getResources(packageName.replace(".", "/"));
-			while (urls.hasMoreElements()) {
-				URL url = urls.nextElement();
-				if (null != url) {
-					String protocol = url.getProtocol();
-					if ("file".equals(protocol)) {
-						String packagePath = url.getPath().replace("%20", " ");
-						addClass(classSet, packagePath, packageName);
-					} else if ("jar".equals(protocol)) {
-						JarURLConnection jarURLConnection = (JarURLConnection) url
-								.openConnection();
-						if (null != jarURLConnection) {
-							JarFile jarFile = jarURLConnection.getJarFile();
-							if (null != jarFile) {
-								Enumeration<JarEntry> jarEntries = jarFile
-										.entries();
-								while (jarEntries.hasMoreElements()) {
-									JarEntry jarEntry = jarEntries
-											.nextElement();
-									String jarEntryName = jarEntry.getName();
-									if (jarEntryName.endsWith(".class")) {
-										String className = jarEntryName
-												.substring(0, jarEntryName
-														.lastIndexOf("."))
-												.replaceAll("/", ".");
-										doAddClass(classSet, className);
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		} catch (IOException e) {
-			LOGGER.error("get class set failure", e);
-			e.printStackTrace();
-		}
-		return null;
-	}
+    /**
+     * 获取指定包名下的所有类
+     *
+     * @param packageName
+     * @return
+     */
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<>();
+        try {
+            Enumeration<URL> urls = getClassLoader()
+                    .getResources(packageName.replace(".", "/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (null != url) {
+                    String protocol = url.getProtocol();
+                    if ("file".equals(protocol)) {
+                        String packagePath = url.getPath().replace("%20", " ");
+                        addClass(classSet, packagePath, packageName);
+                    } else if ("jar".equals(protocol)) {
+                        JarURLConnection jarURLConnection = (JarURLConnection) url
+                                .openConnection();
+                        if (null != jarURLConnection) {
+                            JarFile jarFile = jarURLConnection.getJarFile();
+                            if (null != jarFile) {
+                                Enumeration<JarEntry> jarEntries = jarFile
+                                        .entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries
+                                            .nextElement();
+                                    String jarEntryName = jarEntry.getName();
+                                    if (jarEntryName.endsWith(".class")) {
+                                        String className = jarEntryName
+                                                .substring(0, jarEntryName
+                                                        .lastIndexOf("."))
+                                                .replaceAll("/", ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error("get class set failure", e);
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	private static void doAddClass(Set<Class<?>> classSet, String className) {
-		Class<?> cls = loadClass(className, false);
-		classSet.add(cls);
-	}
+    private static void doAddClass(Set<Class<?>> classSet, String className) {
+        Class<?> cls = loadClass(className, false);
+        classSet.add(cls);
+    }
 
-	private static void addClass(Set<Class<?>> classSet, String packagePath,
-			String packageName) {
+    private static void addClass(Set<Class<?>> classSet, String packagePath,
+                                 String packageName) {
 
-		File[] files = new File(packagePath).listFiles(new FileFilter() {
+        File dir = new File(packagePath);
+        if (dir.exists()) {
+            File[] files = dir.listFiles(file -> (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory());
+            if (files == null) {
+                return;
+            }
+            
+            for (File file : files) {
+                String fileName = file.getName();
+                if (file.isFile()) {
+                    String className = fileName.substring(0,
+                            fileName.lastIndexOf("."));
+                    if (StringUtil.isNotEmpty(packageName)) {
+                        className = packageName + "." + className;
+                        doAddClass(classSet, className);
+                    } else {
+                        String subPackagePath = fileName;
+                        if (StringUtil.isNotEmpty(packagePath)) {
+                            subPackagePath = packagePath + "/" + subPackagePath;
+                        }
+                        String subPackageName = fileName;
+                        if (StringUtil.isNotEmpty(packageName)) {
+                            subPackageName = packageName + "." + subPackageName;
+                        }
+                        addClass(classSet, subPackagePath, subPackageName);
+                    }
+                }
+            }
+        }
 
-			@Override
-			public boolean accept(File file) {
-				return (file.isFile() && file.getName().endsWith(".class"))
-						|| file.isDirectory();
-			}
-		});
-		for (File file : files) {
-			String fileName = file.getName();
-			if (file.isFile()) {
-				String className = fileName.substring(0,
-						fileName.lastIndexOf("."));
-				if (StringUtil.isNotEmpty(packageName)) {
-					className = packageName + "." + className;
-					doAddClass(classSet, className);
-				} else {
-					String subPackagePath = fileName;
-					if (StringUtil.isNotEmpty(packagePath)) {
-						subPackagePath = packagePath + "/" + subPackagePath;
-					}
-					String subPackageName = fileName;
-					if (StringUtil.isNotEmpty(packageName)) {
-						subPackageName = packageName + "." + subPackageName;
-					}
-					addClass(classSet, subPackagePath, subPackageName);
-				}
-			}
-		}
-	}
+    }
 }
