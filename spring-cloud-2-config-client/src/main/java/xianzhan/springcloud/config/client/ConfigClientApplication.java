ada39437b1a61a8c3ee1728754bfd8a296acc11a
xianzhan <424447832@qq.com>
2018-09-26 00:00:51 +0800

    update: spring cloud config

--- spring-cloud-2-config-client/src/main/java/xianzhan/springcloud/config/client/ConfigClientApplication.java
+++ spring-cloud-2-config-client/src/main/java/xianzhan/springcloud/config/client/ConfigClientApplication.java
@@ -3,15 +3,19 @@ package xianzhan.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * RefreshScope 注解，需要访问 /refresh 刷新已更新的配置
+ *
  * @author xianzhan
  * @since 2018-09-25
  */
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class);
