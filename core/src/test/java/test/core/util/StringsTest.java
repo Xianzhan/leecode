a6780a6e7efbc642ae36f675dda72f631a7d0b3f
Xianzhan <424447832@qq.com>
2019-01-02 22:57:57 +0800

    delete

--- core/src/test/java/test/core/util/StringsTest.java
+++ core/src/test/java/test/core/util/StringsTest.java
@@ -3,6 +3,11 @@ package test.core.util;
 import org.junit.Test;
 import xianzhan.core.util.Strings;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author xianzhan
  * @since 2018-11-11
@@ -34,5 +39,14 @@ public class StringsTest {
 
         System.out.println(Strings.isDouble("++"));
         System.out.println(Strings.isDouble("ad"));
+    }
+
+    public static void main(String[] args) {
+        List<String> collect = Stream.of("hello", "world")
+                .map(word -> word.split(""))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .collect(Collectors.toList());
+        collect.forEach(System.out::println);
     }
 }
