e129ae28f13ec90937171bc906170a174b9f0af6
Xianzhan <424447832@qq.com>
2019-01-14 22:49:36 +0800

    add: i18n

---
+++ core/src/main/java/xianzhan/core/util/i18n/Direction.java
@@ -0,0 +1,15 @@
+package xianzhan.core.util.i18n;
+
+/**
+ * 描述：国际化, 方向
+ *
+ * @author Lee
+ * @since 2019-01-14
+ */
+public class Direction {
+
+    public static final String UP    = "direction.up";
+    public static final String DOWN  = "direction.down";
+    public static final String LEFT  = "direction.left";
+    public static final String RIGHT = "direction.right";
+}
---
+++ core/src/main/java/xianzhan/core/util/i18n/I18N.java
@@ -0,0 +1,31 @@
+package xianzhan.core.util.i18n;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * 描述：国际化
+ *
+ * @author Lee
+ * @since 2019-01-14
+ */
+public class I18N {
+
+    private static Locale defaultLocale = Locale.getDefault();
+
+    private static ResourceBundle bundle;
+
+    public static void setResource(String basename) {
+        bundle = ResourceBundle.getBundle(basename, defaultLocale);
+    }
+
+    public static void setResource(String basename, Locale locale) {
+        bundle = ResourceBundle.getBundle(basename, locale);
+    }
+
+    public static void main(String[] args) {
+        setResource("i18n.direction");
+        String string = bundle.getString(Direction.UP);
+        System.out.println(string);
+    }
+}
