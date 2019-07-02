34e70145672ec35815ffea8e21bb7ca2f1fe8644
xianzhan <424447832@qq.com>
2019-07-02 21:24:11 +0800

    :heavy_plus_sign: NumberUtils.add/sub/mul/div method

--- core/src/test/java/test/core/util/NumberUtilsTest.java
+++ core/src/test/java/test/core/util/NumberUtilsTest.java
@@ -13,6 +13,10 @@ public class NumberUtilsTest {
     public static void main(String[] args) {
         testIsPowerOfTwo();
         testIsOdd();
+        testAdd();
+        testSub();
+        testMul();
+        testDiv();
     }
 
     private static void testIsPowerOfTwo() {
@@ -25,5 +29,37 @@ public class NumberUtilsTest {
         for (int i = -10; i < 10; i++) {
             System.out.println(i + "\t is odd: " + NumberUtils.isOdd(i));
         }
+    }
+
+    private static void testAdd() {
+        int sum = 0;
+        for (int i = 1; i < 100; i++) {
+            sum = NumberUtils.add(sum, i);
+        }
+        System.out.println("1 + ... + 99 = " + sum);
+    }
+
+    private static void testSub() {
+        int sum = 4950;
+        for (int i = 1; i < 100; i++) {
+            sum = NumberUtils.sub(sum, i);
+        }
+        System.out.println("4950 - ... - 1 = " + sum);
+    }
+
+    private static void testMul() {
+        int mul = 1;
+        for (int i = 1; i < 10; i++) {
+            mul = NumberUtils.mul(mul, i);
+        }
+        System.out.println("1 * ... * 9 = " + mul);
+    }
+
+    private static void testDiv() {
+        int div = 362880;
+        for (int i = 1; i < 10; i++) {
+            div = NumberUtils.div(div, i);
+        }
+        System.out.println("362880 / ... / 1 = " + div);
     }
 }
